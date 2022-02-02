66890c14f3c6208cdb73e541fd2a3949885d4bc5
Aleks Bekker <alekso.bekker@gmail.com>
2022-02-01 22:54:35 -0700

    Standardize database code

--- source/recognized_faces_database.m
+++ source/recognized_faces_database.m
@@ -1,46 +1,22 @@
-load ../faces/
+function unique_id = create_unique_id(faces_map)
+    
+    % find maximum ID number
+    max_id = 2 ^ 12;
+    while max_id < size(faces_map, 1) / 2
+        max_id = max_id * 2;
+    end
 
-faces = zeros(n*6,m*6);
+    % find random, unused ID number
+    id = random('Uniform', 0, max_id);
+    while not isKey(faces_map, id)
+        id = random('Uniform', 0, max_id);
+    end
 
+    unique_id = id;
 
-personal;
-eigenweights;
-
-[U,S,V] = svd(faces,'econ');
-
-W = U'*x;
-count = 1
-for r = [25 50 100 200 400 800 1600];%Go through each rank for each image 
-    personal = containers.Map({0,1,2,3,4,5,6,7,8,9,10},{ 'Eshann', 'Kwame', 'Adrian', 'Juliana', 'Jake', 'Jane', 'Aleks', 'Alex CC', 'Mostafa', 'Rahul','Mr C'});
-    eigenweights = containers.Map({0,1,2,3,4,5,6,7,8,9,10},{U(:,1:r)*U(:,1:r)'*x});
-    count = count + 1;
-
-end;
-
-
-
-function create_new_entry(personal_map, eigenweights_map, new_name, new_eigen_weights)
-    unique_key = [keys.personal, keys.eigenweights];
-    personal = personal_map;
-    eigenweights = eigenweights_map;
-
-    return unique_key;
-end;
-
-some_array = [];
-function add_person(name,weights);
-count = 1;
-for i = 1:personal.length;
-    for j = 1:eigenweights.length;%Loop through each map to 
-    somearray.add(create_new_entry(personal,eigenweights,personal{i},eigenweights{j}));
-    count = count + 1;
-    %Creates a new entry for each person
-    end;
-end;
-return somearray;
 end
 
-for x = 1:personal.length;
-    add_person(personal{x},W);
-    count = count + 1;
-end;
+function id = create_new_entry(faces_map, name, weights)
+    personal_map = containers.Map({"name", "weights"}, {name, weights});
+    add(faces_map, create_unique_id(faces_map), personal_map)
+end
